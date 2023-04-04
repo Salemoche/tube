8b3bbe16395c4fc2250c6cb547121e86eaafd819
Gabriel Bach <gabrielmolembi@hotmail.com>
2023-04-04 17:57:03 +0200

    reinitialize next 13

---
+++ .slicemachine/assets/slices/ExhibitionSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/ExhibitionSlice';
+
+export default {
+  title: 'slices/ExhibitionSlice'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Mental","spans":[]}],"description":[{"type":"paragraph","text":"Et do fugiat irure nisi exercitation magna et esse magna laborum dolore cupidatat. Voluptate nostrud Lorem occaecat sunt et occaecat Lorem sit aute duis.","spans":[]}]},"id":"_Default","slice_type":"exhibition_slice"}} />
+_Default.storyName = ''
